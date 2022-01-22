af877794a959e52c407a7224a8fbb2f51e6cdc89
Jacob Schaer <jacobschaer@users.noreply.github.com>
2022-01-22 14:42:14 -0800

    Version update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="doipclient",
-    version="1.0.4",
+    version="1.0.5",
     description="A Diagnostic over IP (DoIP) client implementing ISO-13400-2.",
     long_description=long_description,
     author="Jacob Schaer",
