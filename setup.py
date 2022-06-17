d7c69444bc02e8f7a7edacbeb7c6c5a20e36c726
Jacob Schaer <jacobschaer@users.noreply.github.com>
2022-06-16 21:26:18 -0700

    Update setup.py
    New version for socket fix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="doipclient",
-    version="1.0.6",
+    version="1.0.7",
     description="A Diagnostic over IP (DoIP) client implementing ISO-13400-2.",
     long_description=long_description,
     author="Jacob Schaer",
