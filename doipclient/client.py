aba65bdfd3e3100a9e9f3dc5e595ce5349e4e421
Jakub Krista <jakub.krista@digiteqautomotive.com>
2025-07-24 13:52:50 +0200

    Remove old 'log_level' parameter docstring from DoIPClient.__init__; add 'vm_specific' as an init argument and store it as a static value for the entire instance. Add .idea/ to .gitignore

--- doipclient/client.py
+++ doipclient/client.py
@@ -145,10 +145,10 @@ class DoIPClient:
     :param use_secure: Enables TLS. If set to True, a default SSL context is used. For more control, a preconfigured
         SSL context can be passed directly. Untested. Should be combined with changing tcp_port to 3496.
     :type use_secure: Union[bool,ssl.SSLContext]
-    :param log_level: Logging level
-    :type log_level: int
     :param auto_reconnect_tcp: Attempt to automatically reconnect TCP sockets that were closed by peer
     :type auto_reconnect_tcp: bool
+    :param vm_specific: Optional 4 byte long int
+    :type vm_specific: int, optional
 
     :raises ConnectionRefusedError: If the activation request fails
     :raises ValueError: If the IPAddress is neither an IPv4 nor an IPv6 address
@@ -166,6 +166,7 @@ class DoIPClient:
         client_ip_address=None,
         use_secure=False,
         auto_reconnect_tcp=False,
+        vm_specific=None
     ):
         self._ecu_logical_address = ecu_logical_address
         self._client_logical_address = client_logical_address
@@ -180,6 +181,7 @@ class DoIPClient:
         self._protocol_version = protocol_version
         self._auto_reconnect_tcp = auto_reconnect_tcp
         self._tcp_close_detected = False
+        self.vm_specific = vm_specific
 
         # Check the ECU IP type to determine socket family
         # Will raise ValueError if neither a valid IPv4, nor IPv6 address
@@ -589,7 +591,7 @@ class DoIPClient:
         :rtype: RoutingActivationResponse
         """
         message = RoutingActivationRequest(
-            self._client_logical_address, activation_type, vm_specific=vm_specific
+            self._client_logical_address, activation_type, vm_specific=vm_specific or self._vm_specific
         )
         self.send_doip_message(message, disable_retry=disable_retry)
         while True:
@@ -824,3 +826,24 @@ class DoIPClient:
                 raise ConnectionRefusedError(
                     f"Activation Request failed with code {result.response_code}"
                 )
+
+    @property
+    def vm_specific(self):
+        """Get the optional OEM specific field value if set.
+
+        :return: vm_specific value
+        :rtype: int, optional
+        """
+        return self._vm_specific
+
+    @vm_specific.setter
+    def vm_specific(self, value):
+        """Set the optional OEM specific field value. If you do not need to send this item, set it to None.
+
+        :param value: The vm_specific value (must be > 0 and <= 0xffffffff) or None.
+        :type value: int, optional
+        :raises ValueError: Value is invalid or out of range
+        """
+        if value < 0 or value > 0xffffffff or value is None:
+            raise ValueError("Invalid VM specific value must be > 0 and <= 0xffffffff or None")
+        self._vm_specific = value
