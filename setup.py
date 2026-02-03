1b6a891208c98b295a96391a8e08664ee54dd007
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-02-03 17:46:30 -0500

    build: Transition completely to pyproject.toml

--- setup.py
+++ setup.py
@@ -2,34 +2,4 @@
 
 import setuptools
 
-with open("README.rst", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="doipclient",
-    version="1.1.8",
-    description="A Diagnostic over IP (DoIP) client implementing ISO-13400-2.",
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    author="Jacob Schaer",
-    url="https://github.com/jacobschaer/python-doipclient",
-    packages=["doipclient"],
-    package_data={"doipclient": ["py.typed"]},
-    keywords=[
-        "uds",
-        "14229",
-        "iso-14229",
-        "diagnostic",
-        "automotive",
-        "13400",
-        "iso-13400",
-        "doip",
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
-    ],
-    python_requires=">=3.6",
-)
+setuptools.setup()
